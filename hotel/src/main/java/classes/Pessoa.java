b9fb41cd7e2d21ed183a438b0dc3123345c8d023
joao <joaovmiguez@gmail.com>
2023-08-25 11:03:38 -0300

    Create Pessoa class

---
+++ hotel/src/main/java/classes/Pessoa.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class Pessoa {
+    private String nomePessoa;
+    private String email;
+    private int cpf;
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+    public void setNomePessoa(String nomePessoa) {
+        this.nomePessoa = nomePessoa;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
