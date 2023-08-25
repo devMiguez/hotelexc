d840431ef807e527e060859eb08f6ca1562eecdc
joao <joaovmiguez@gmail.com>
2023-08-25 10:36:20 -0300

    Create hotel class

---
+++ hotel/src/main/java/classes/Hotel.java
@@ -0,0 +1,31 @@
+package classes;
+
+import java.util.Scanner;
+
+public class Hotel {
+    private String nomeHotel;
+    private String endereco;
+    private String cnpj;
+
+
+    public String getNomeHotel() {
+        return nomeHotel;
+    }
+    public void setNomeHotel(String nomeHotel) {
+        this.nomeHotel = nomeHotel;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+}
